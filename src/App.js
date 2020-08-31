24305b18f3e291b789132afd6ad276aaf105a178
Reinoptland <reinoptland@gmail.com>
2020-08-31 14:00:59 +0200

    Add git branch rules on homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>How should we us branches </li>
+          <li>
+            Master: Contains the current working version (bug free & tested){" "}
+          </li>
+          <li>
+            Development branch: Where we combine our work (can be broken
+            sometimes){" "}
+          </li>
+          <li>Feature branch: contains isolated new features</li>
+        </ul>
       </header>
     </div>
   );
